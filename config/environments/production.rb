51b4282002e92d9993e0f6c786b7d1ed4622c52f
Guy Issler <guyissler@gmail.com>
2014-05-26 23:02:22 +0300

    change production settings for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Coverletter::Application.configure do
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      'guyissler@gmail.com',
     :password =>       '7fyvuVlG1DA4Ah9kPsdTbQ',
-    :domain =>         'c66.me',
+    :domain =>         'www.myspitch.com',
     :authentication => :plain
   }
   ActionMailer::Base.delivery_method = :smtp
