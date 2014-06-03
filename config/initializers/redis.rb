cc2efdc2db9d46873d81f51f1f138189a4f4673d
Guy Issler <guyissler@gmail.com>
2014-06-03 16:54:41 +0300

    redis empty password

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,4 +6,4 @@
 #end
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => "")
