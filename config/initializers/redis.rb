76b942d86462a851ce3cbfc2832e23c25edb940a
Guy Issler <guyissler@gmail.com>
2014-06-03 16:50:29 +0300

    added uri.password to redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -6,4 +6,4 @@
 #end
 
 uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
